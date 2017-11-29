bb45a16ac585a6e3aee9dfdbc0fc077d77511f4c
Frank Lam <frank.is.missing@gmail.com>
2017-11-29 19:38:33 +0000

    Completed position.js

--- position.js
+++ position.js
@@ -1,22 +1,27 @@
 function position(word){
   var string = word.split(" ").join("");
   var counter = {};
-  //for (var letter in string){
     for (var i = 0; i < string.length; i++){
     letter = string[i];
     // console.log("current working with letter: " + letter);
       if (counter[letter] === undefined){
-        counter[letter] = 0;
+        counter[letter] = [];
       }
-      counter[letter] += 1;
+      counter[letter].push(i);
   }
   console.log(counter);
 }
 
 position("lighthouse in the house");
 
-// iterate through the word for unique letters
-// if the value is < 1, than create a new value
-// else create a value of 0
-// if the letter is unique, then assign it as a key with value of 1
-//     if not unique
+/*
+if key property doesn't exist - create it
+
+
+l: 1, 2, 3
+i:
+g:
+h:
+t:
+
+*/
